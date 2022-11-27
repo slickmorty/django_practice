ada8a71e862cf6423848ed90d8351f1543429c38
Ahmad Abasi <a_abasi97@yahoo.com>
2022-11-28 01:46:01 +0330

    54:46

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,17 @@
 from django.http import JsonResponse
+from products.models import Product
 import json
 
 
 def api_home(request, *args, **kwargs):
 
-    print(f"----------{request.GET=}")
-    print(f"----------{request.POST=}")
-
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(request.body)
-    except Exception:
-        pass
 
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data["content_type"] = request.content_type
+    if model_data:  # if model data is there
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
 
     return JsonResponse(data)
